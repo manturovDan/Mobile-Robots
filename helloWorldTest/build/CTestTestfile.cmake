7b03db01b563044bba88c11e0d3f980a0755cc92
manturov <manturov.dan@mail.ru>
2019-09-07 18:15:23 +0300

    second

--- helloWorldTest/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/danila/source/labs3seb/helloWorldTest/src
-# Build directory: /home/danila/source/labs3seb/helloWorldTest/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(HelloWorldTest "/home/danila/source/labs3seb/helloWorldTest/build/hello_world_test")
