8d95d0394885b5c8217217b079ef2b283ff5875f
manturov <manturov.dan@mail.ru>
2019-09-07 18:11:06 +0300

    first commit

---
+++ helloWorldTest/build/CMakeFiles/hello_world_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_world_test.dir/test.cpp.o"
+  "hello_world_test.pdb"
+  "hello_world_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello_world_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
