272c4237c85dfcdc1b5cf5d2344ad8a38bf4c8a4
manturov <manturov.dan@mail.ru>
2019-09-13 08:20:33 +0300

    prot

--- matrix/src/main.cpp
+++ matrix/src/main.cpp
@@ -9,22 +9,27 @@ int main() {
 	int cols;
 	Line *mat;
 	int getm = inputM(mat, cols, rows);
-	
+	if (getm < 0) {
+		std::cout<<"Input error!"<<std::endl;
+		return 1;
+	}
+
 	std::cout << "Original matrix:" << std::endl;
 	outputM(mat, cols, rows);
 	std::cout << std::endl;
 
 	Line *conv;
 	int filr = convertM(conv, mat, rows, cols, getm);
-	//sortM(conv, rows - filr, getm - filr);
+	if (filr == -2)
+		return 1;
 
 	std::cout << "Result matrix:" << std::endl;
 	outputM(conv, cols, rows - filr);
 
-	std::cout<<std::endl;
-	printTable(mat);
-	std::cout<<std::endl;
-	printTable(conv);
+	//std::cout<<std::endl;
+	//printTable(mat);
+	//std::cout<<std::endl;
+	//printTable(conv);
 
 	eraseM(mat);
 	eraseMA(conv);
