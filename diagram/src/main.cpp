36c9ac0d11393974e51220cf2d0699d2e21fb2a0
manturovDan <manturov.dan@mail.ru>
2019-10-07 00:18:46 +0300

    interface

--- diagram/src/main.cpp
+++ diagram/src/main.cpp
@@ -28,10 +28,11 @@ int chooseAct() {
 	3. Replace the first diagram on the second (from definetely time)\n\
 	4. Copy the diagram n times\n\
 	5. Shift the diagram\n\
+	6. Print diagram\n\
 	0. Exit" << std:: endl;	
 		
 		inpSmt(choise);
-		if(choise >= 0 && choise <= 5)
+		if(choise >= 0 && choise <= 6)
 			return choise;
 
 		std::cout << "Incorrect value!" << std::endl;
@@ -92,9 +93,119 @@ int launchFunc(timeD::Diagram &diag1, timeD::Diagram &diag2, int act) {
 			std::cout << "Successful adding" << std::endl;
 			return 0;
 		}
-		else {
+		else if (act == 2) {
+			int diagLeft = chooseDiag(2, "Choose the root diagram");
+			int diagRight = chooseDiag(2, "Choose diagram which to add");
+
+			timeD::Diagram *root;
+			timeD::Diagram *add;
+			if (diagLeft == 1)
+				root = &diag1;
+			else
+				root = &diag2;
+
+			if (diagRight == 1)
+				add = &diag1;
+			else
+				add = &diag2;
+
+			if(root->uniDiagram(*add)) {
+				std::cout << "Unsuccessfull union" << std::endl;
+				return 1;
+			}
+
+			std::cout << "Successfull union" << std::endl;
+			return 0;
 
 		}
+		else if (act == 3) {
+			int diagLeft = chooseDiag(2, "Choose the root diagram");
+			int diagRight = chooseDiag(2, "Choose diagram which to add");
+
+			timeD::Diagram *root;
+			timeD::Diagram *add;
+			if (diagLeft == 1)
+				root = &diag1;
+			else
+				root = &diag2;
+
+			if (diagRight == 1)
+				add = &diag1;
+			else
+				add = &diag2;
+
+			std::cout << "Input moment of time from which root diagram will be changed" << std::endl;
+			int moment;
+			inpSmt(moment);
+
+			try {
+				root->replace(moment, *add);
+			} catch (std::exception &ex) {
+				std::cout << ex.what() << std::endl;
+				return 1;
+			}
+
+			std::cout << "Successfull replacing" << std::endl;
+			return 0;
+		}
+		else if (act == 4) {
+			int diag = chooseDiag(2, "Choose diagram to copy");
+			
+			timeD::Diagram *work;
+			if (diag == 1)
+				work = &diag1;
+			else
+				work = &diag2;
+
+			std::cout << "Input count of copies" << std::endl;
+			int ccnt;
+			inpSmt(ccnt);
+
+			if(work->copyDiagram(ccnt)) {
+				std::cout << "Error. Diagram will be too big" << std::endl;
+				return 1;
+			}
+
+			std::cout << "Successfull copying" << std::endl;
+			return 0;
+		}
+		else if (act == 5) {
+			int diag = chooseDiag(2, "Choose diagram to shift");
+			
+			timeD::Diagram *work;
+			if (diag == 1)
+				work = &diag1;
+			else
+				work = &diag2;
+
+			std::cout << "Input number of shift (+/-)" << std::endl;
+			int shf;
+			inpSmt(shf);
+
+			if(work->shift(shf)) {
+				std::cout << "Error while shifting" << std::endl;
+				return 1;
+			}
+
+			std::cout << "Successfull shifting" << std::endl;
+			return 0;
+		}
+		else if (act == 6) {
+			int diag = chooseDiag(2, "Choose diagram to print");
+
+			timeD::Diagram *work;
+			if (diag == 1)
+				work = &diag1;
+			else
+				work = &diag2;
+
+			work->printDiagram(std::cout);
+			work->printSignals(std::cout);
+
+			return 0;
+		}
+
+		return -1;
 }
 
 int main() {
