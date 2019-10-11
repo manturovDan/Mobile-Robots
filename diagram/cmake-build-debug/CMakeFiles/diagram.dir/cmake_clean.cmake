f5aa826312ad2a30c7e1589ce1867cfa112e66d7
manturovDan <manturov.dan@mail.ru>
2019-10-11 21:57:20 +0300

    new pr

---
+++ diagram/cmake-build-debug/CMakeFiles/diagram.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/diagram.dir/main.cpp.o"
+  "diagram"
+  "diagram.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/diagram.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
