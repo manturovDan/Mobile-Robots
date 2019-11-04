8f272ff0d5c0013eb7843408879a3216f1879d21
manturovDan <manturov.dan@mail.ru>
2019-11-04 15:37:19 +0300

    tests for file-working + all bind

---
+++ diagram/cmake-build-debug/CMakeFiles/a_test.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/danila/source/labs3sem/diagram/app_test/test.cpp" "/home/danila/source/labs3sem/diagram/cmake-build-debug/CMakeFiles/a_test.dir/app_test/test.cpp.o"
+  "/home/danila/source/labs3sem/diagram/src/diagfile.cpp" "/home/danila/source/labs3sem/diagram/cmake-build-debug/CMakeFiles/a_test.dir/src/diagfile.cpp.o"
+  "/home/danila/source/labs3sem/diagram/src/diagram.cpp" "/home/danila/source/labs3sem/diagram/cmake-build-debug/CMakeFiles/a_test.dir/src/diagram.cpp.o"
+  "/home/danila/source/labs3sem/diagram/tinyxml2-lib/tinyxml2.cpp" "/home/danila/source/labs3sem/diagram/cmake-build-debug/CMakeFiles/a_test.dir/tinyxml2-lib/tinyxml2.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ diagram/cmake-build-debug/CMakeFiles/a_test.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/a_test.dir/app_test/test.cpp.o"
+  "CMakeFiles/a_test.dir/src/diagfile.cpp.o"
+  "CMakeFiles/a_test.dir/src/diagram.cpp.o"
+  "CMakeFiles/a_test.dir/tinyxml2-lib/tinyxml2.cpp.o"
+  "a_test"
+  "a_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/a_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
