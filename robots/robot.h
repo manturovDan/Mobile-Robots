49fbc05d81d4f9c20e7f430ccfa2145f86d1fdc7
manturovDan <manturov.dan@mail.ru>
2019-11-10 14:10:10 +0300

    header file

--- robots/robot.h
+++ robots/robot.h
@@ -1,12 +1,14 @@
 #ifndef ROBOTSCREATE_ROBOT_H
 #define ROBOTSCREATE_ROBOT_H
 
+#include <vector>
+
+#include <string>
+
 namespace robo {
     struct map_position {
         unsigned int x;
         unsigned int y;
-        unsigned int x_size;
-        unsigned int y_size;
         int direction;
     };
 
@@ -28,15 +30,116 @@ namespace robo {
     class Map_Object {
     private:
         map_position position;
+    protected:
+        Map_Object() {}
     public:
         map_position getPosition();
     };
 
-    class Obstacle : public Map_Object {};
+    class Obstacle : public Map_Object {
+    public:
+        Obstacle() {}
+    };
 
-    class Interest_Point : public Map_Object {};
+    class Interest_Point : public Map_Object {
+    public:
+        Interest_Point() {}
+    };
 
-    class Robo_Component : public Map_Object {};
+    class Module;
+
+    class Robo_Component : public Map_Object {
+    protected:
+        Robo_Component() {}
+
+        map_position position;
+        std::string description;
+        int energyConsumption;
+        int cost;
+        int countPorts;
+        std::vector<Module *> modules;
+    public:
+        std::string getDescription();
+        int getEnergyConsumption();
+        int getCost();
+        int getCountPorts();
+        map_position getPosition();
+    };
+
+    class Mobile : Robo_Component {
+    protected:
+        Mobile ();
+        int speed;
+    public:
+        int getSpeed();
+        int move(int);
+        int turn(int);
+    };
+
+    class Manager : public Robo_Component {
+    protected:
+        Manager ();
+    };
+
+    class Robot_Scout : public Mobile {
+    public:
+        Robot_Scout();
+    };
+
+    class Robot_Commander : public Robot_Scout, public Manager { //make virtual
+    public:
+        Robot_Commander();
+    };
+
+    class Observation_Center : public Robo_Component {
+    public:
+        Observation_Center();
+    };
+
+    class Command_Center : public Observation_Center, public Manager {
+        Command_Center();
+    };
+
+    class Module {
+    protected:
+        Module();
+        int priority;
+        bool active;
+    public:
+        int getPriority();
+        bool getActive();
+        int setActive();
+    };
+
+    class Energy_Consumer {
+    protected:
+        Energy_Consumer();
+        int powerConsumption;
+    };
+
+    class Power_Generator {
+    public:
+        Power_Generator();
+    protected:
+        int energyProduction;
+    };
+
+    class Managing : public Energy_Consumer {
+    public:
+        Managing();
+    protected:
+        std::vector<Robo_Component *> subordinate;
+    };
+
+    class Sensor : public Energy_Consumer {
+    public:
+        Sensor();
+    protected:
+        int radius;
+        int direction;
+        int angle;
+
+    };
 }
 
 #endif //ROBOTSCREATE_ROBOT_H
