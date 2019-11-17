7b8f36f9aad0cfd9bef6be8b0d98c0c5fdc280d1
manturovDan <manturov.dan@mail.ru>
2019-11-17 17:29:08 +0300

    new branch start

---
+++ multimap/cmake-build-debug/CMakeFiles/multimapTest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/multimapTest.dir/main.cpp.o"
+  "multimapTest"
+  "multimapTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/multimapTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
