ecdf0b747392780bd0badabe15528260812791c4
manturovDan <manturov.dan@mail.ru>
2019-12-17 00:11:02 +0300

    dialog was redone

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include "robots/Ai_Deep.h"
 
 template <class T>
-int inpNum(T &inp, bool unneg = false, const char *again = "Input error! Try again!") {
+int inpSmt(T &inp, const char *again = "Input error! Try again!") {
     bool more = false;
     while (1) {
         if (more)
@@ -19,28 +19,9 @@ int inpNum(T &inp, bool unneg = false, const char *again = "Input error! Try aga
             more = 1;
             continue;
         }
-        if(std::abs(inp) > 100000000 || (unneg && inp < 0))
-            more = true;
+
         else
             return 0;
-    }
-}
-
-int inpString(std::string &inp, const char *again, std::ostream &stream) {
-    bool more = false;
-    while (1) {
-        if (more)
-            std::cout << again << std::endl;
-
-        std::cin >> inp;
-        if (!std::cin.good()) {
-            std::cin.clear();
-            std::cin.ignore(100, '\n');
-            more = 1;
-            continue;
-        }
-
-        return 0;
     }
 }
 
@@ -49,12 +30,10 @@ int chooseAct() {
     while(1) {
         std::cout << "Choose the number\n\
     0. Exit\n\
-    1. Generate environment\n\
-    2. Load environment from xml\n\
-    3. Create environment" << std::endl;
+    1. Load environment from xml" << std::endl;
 
-        inpNum(choise);
-        if (choise >= 0 && choise <= 3)
+        inpSmt(choise);
+        if (choise >= 0 && choise <= 1)
             return choise;
 
         std::cout << "Incorrect value! Try again!\n" << std::endl;
@@ -63,33 +42,31 @@ int chooseAct() {
 
 int launchFunc(int act, robo::Environment_describer * env) {
     if (act == 1) {
-        return 0;
-    } else if (act == 2) {
-        interf::EnvXMLCreate("/home/danila/source/robots/storage/map6.xml", *env);
-        auto itf = interf::InterfaceTools(env);
-        auto ai = robo::Ai_Deep(env);
-        robo::Managing::setAI(&ai);
+        std::cout << "Input path to XML-file of map" << std::endl;
+        std::string fileName;
+        inpSmt(fileName);
+        try {
+            interf::EnvXMLCreate(fileName, *env);
+            auto itf = interf::InterfaceTools(env);
+            auto ai = robo::Ai_Deep(env);
+            robo::Managing::setAI(&ai);
 
-        //itf.printEnv();
-        itf.printEnv();
-        //ai.print(std::cout);
-
-        dispr::Display disp(env, & ai);
-        disp.run();
-        return 0;
-    } else if (act == 3) {
-        env->setHeight(15);
-        env->setWidth(20);
-        return 0;
+            dispr::Display disp(env, &ai);
+            disp.run();
+            return 0;
+        }
+        catch (std::exception &ex) {
+            std::cerr << ex.what() << std::endl << "Try again!\n" << std::endl;
+        }
     }
+
+    return 1;
 }
 
 int main()
 {
     robo::Environment_describer env;
-    //robo::Map_Object *obs1 = env.setObject({10, 10, 1}, "Obstacle");
 
-    //std::cout << "MOBILE ROBOTS ONE" << std::endl;
     std::cout << "\n __  __  ____  ____ _____ _      ______   _____   ____  ____   ____ _______ _____ \n"
                  "|  \\/  |/ __ \\|  _ \\_   _| |    |  ____| |  __ \\ / __ \\|  _ \\ / __ \\__   __/ ____|\n"
                  "| \\  / | |  | | |_) || | | |    | |__    | |__) | |  | | |_) | |  | | | | | (___  \n"
